5cac5df5e0bb72492f258cceb09f93bff0d3a057
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-02-12 08:45:22 -0700

    Add starting routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      resources :foods, only: [:index]
+      resources :reactions, only: [:index]
+    end
+  end
 end
