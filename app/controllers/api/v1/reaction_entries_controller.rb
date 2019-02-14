8547f71452ecdeebeccb0836329d58fdfef3ca9b
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-02-13 17:42:23 -0700

    Add reaction_entries_conroller#create

--- app/controllers/api/v1/reaction_entries_controller.rb
+++ app/controllers/api/v1/reaction_entries_controller.rb
@@ -5,4 +5,14 @@ class Api::V1::ReactionEntriesController < ApplicationController
     render json: ReactionEntrySerializer.new(reaction_entries)
   end
 
+  def create
+    begin
+      re = ReactionEntry.new(reaction_id: params[:reaction_id])
+      re.time = params[:time]
+      re.save!
+      render json: {'status': "Entered reaction at #{params[:time]}"}, status: 201
+    rescue
+      render status: 400
+    end
+  end
 end
