c6ca8272ae565689d022a9766ed42f54160e2394
Quinn Krug <quinn.krug@gmail.com>
2019-02-14 15:04:04 -0700

    Update reaction POST request to include ids and filter duplicates

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -1,4 +1,9 @@
 class Reaction < ApplicationRecord
+
+  before_validation do |reaction|
+    reaction.name = reaction.name.downcase.titleize if attribute_present? 'name'
+  end
+
   validates_presence_of :name
   validates :name, uniqueness: true
   has_many :reaction_entries
--- spec/requests/api/v1/reactions_request_spec.rb
+++ spec/requests/api/v1/reactions_request_spec.rb
@@ -29,6 +29,34 @@ RSpec.describe '/api/v1/reactions' do
       expect(status).to eq("Created Headache")
       expect(Reaction.count).to eq(1)
     end
+
+    it 'wont create a reaction if the body is empty' do
+      post endpoint
+
+      expect(response.status).to eq(400)
+
+      status = JSON.parse(response.body)['status']
+
+      expect(status).to eq("params are empty")
+      expect(Reaction.count).to eq(0)
+    end
+
+    it 'wont create a duplicate reaction' do
+      reaction = Reaction.create(name: "Headache")
+      name = "HeAdAcHe"
+      headache_id = reaction.id
+      post "#{endpoint}?name=#{name}"
+
+      expect(response.status).to eq(409)
+
+      status = JSON.parse(response.body)['status']
+      id = JSON.parse(response.body)['id']
+
+      expect(status).to eq("#{name} is a duplicate")
+      expect(id).to eq(headache_id)
+      expect(Reaction.count).to eq(1)
+    end
+
   end
 
 end
