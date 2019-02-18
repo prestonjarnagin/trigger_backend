bb94c29bc7714dfa5c037e24da20fcc1ba48a6d6
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-02-17 18:14:41 -0700

    Write .foods method in reaction model. Fix typos

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -8,21 +8,38 @@ class Reaction < ApplicationRecord
   validates :name, uniqueness: true
   has_many :reaction_entries
 
+  def foods
+    times = self.reaction_entries.pluck(:time)
+    ranges = times.map do |time|
+      (time-86400)..time
+    end
+    data = FoodEntry.where(time: [ranges]).pluck(:food_id)
+    reaction_counts = Hash.new(0)
+    data.each do |food_id|
+      reaction_counts[food_id] += 1
+    end
 
+    array = []
+    results = reaction_counts.map do |food_id, reaction_count|
+      frequency = (reaction_count.to_f / FoodEntry.where(food_id: food_id).count).round(2)
+      name = Food.find(food_id).name
+      ReactionSummary.new(food_id, name, frequency)
+    end
+  end
 
-  def occurances
+  def occurences
     {
-      last_7_days: get_occurances_for_range(now - (seconds_in_a_day*7)..now),
-      last_15_days: get_occurances_for_range(now - (seconds_in_a_day*15)..now),
-      last_30_days: get_occurances_for_range(now - (seconds_in_a_day*30)..now),
+      last_7_days: get_occurences_for_range(now - (seconds_in_a_day*7)..now),
+      last_15_days: get_occurences_for_range(now - (seconds_in_a_day*15)..now),
+      last_30_days: get_occurences_for_range(now - (seconds_in_a_day*30)..now),
       last_31_to_60_days:
-        get_occurances_for_range(now - (seconds_in_a_day*60) .. (now - (seconds_in_a_day*30)))
+        get_occurences_for_range(now - (seconds_in_a_day*60) .. (now - (seconds_in_a_day*30)))
     }
   end
 
   private
 
-  def get_occurances_for_range(date_range)
+  def get_occurences_for_range(date_range)
       ReactionEntry
       .where(time: date_range)
       .where(reaction_id: self.id)
