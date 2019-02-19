7e7e9a488427483c5ec5c090081d162c1dfc72fd
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-02-19 11:32:55 -0700

    Update application.rb with logging for Docker

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,10 @@ module TriggerBackend
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.log_tags  = [:subdomain, :uuid]
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 end
